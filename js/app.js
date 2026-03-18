36c041b6b011ed4b1949212fc8860b65309bf9c5
loleardini <loleardini@gmail.com>
2026-03-18 14:47:10 +0100

    Fix API_URL aggiunto /api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://gestione-eventi-backend.onrender.com';
+const API_URL = 'https://gestione-eventi-backend.onrender.com/api';
 let currentUser = null;
 let currentToken = null;
 let currentView = 'login';
